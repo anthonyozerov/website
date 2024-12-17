78de27f8ee0e1f426940d94d9b464aeb23e0d850
anthonyozerov <anthony@ozerov.email>
2024-12-17 05:41:06 -0800

    fix ical script

--- berkeley/calendar/script.sh
+++ berkeley/calendar/script.sh
@@ -1,3 +1,2 @@
 # run all the calendars
-TZ=America/Los_Angeles
-timeout 120 /home/aozerov/.miniconda3/condabin/conda run -n scrape python openrec.py
+TZ=America/Los_Angeles timeout 120 /home/aozerov/.miniconda3/condabin/conda run -n scrape python openrec.py
