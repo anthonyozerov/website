35a73589155381d5b840bcf6a59a68aa13425b86
anthonyozerov <anthony@ozerov.email>
2024-12-18 13:09:08 -0800

    add timezone to weight room script

--- berkeley/weightroom/script.sh
+++ berkeley/weightroom/script.sh
@@ -1,1 +1,1 @@
-timeout 240 /home/aozerov/.miniconda3/condabin/conda run -n scrape python make-figs.py
+TZ=America/Los_Angeles timeout 240 /home/aozerov/.miniconda3/condabin/conda run -n scrape python make-figs.py
